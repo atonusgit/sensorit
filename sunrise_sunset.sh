37aa9d3ab5cce31f15c276e855600809ba4008b4
atonus <sensoritgit@valle.fi>
2022-05-22 22:28:08 +0300

    fix sunrise logic bug

--- sunrise_sunset.sh
+++ sunrise_sunset.sh
@@ -6,7 +6,7 @@ sunrise=$(date --date=$(cat $ROOT_DIRECTORY/sunrise_sunset_logs/$(date '+%y%m%d'
 sunset=$(date --date=$(cat $ROOT_DIRECTORY/sunrise_sunset_logs/$(date '+%y%m%d')_sunrise_sunset.json | jq -r '.results.sunset') +"%s")
 now=$(date +"%s")
 
-if [[ $now -gt $sunrise ]]; then
+if [[ $now -gt $sunrise && $now -lt $sunset ]]; then
         if [[ $(cat $ROOT_DIRECTORY/status_files/all_status.json | jq -r ".G.status") = "is_read_on" ]]; then
 		ssh $PISTORASIAT_USERNAME@$PISTORASIAT_ADDRESS python3 $PISTORASIAT_ROOT_DIRECTORY/remote_control.py G off
         fi
